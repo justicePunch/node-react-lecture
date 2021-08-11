d5f6900b5884a2e77fb2837314f0c56e23fa6014
xxv234 <crossdrive0919@gmail.com>
2021-08-11 10:10:30 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -2,14 +2,34 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://xxv234:drive0919@boilerplate.jqtly.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true, useFindAndModify:false
 }).then(()=>console.log('MongoDB Connected...'))
   .catch(err=>console.log(err))
 
-app.get('/', (req, res) => {
-  res.send('Hello World!안녕하세요')
+app.get('/', (req, res) => res.send('Hello World!안녕하세요~새해복 많이 받으세요'))
+
+app.post('/register', (req, res)=> {
+    
+    //회원가입할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+    
+    const user = new User(req.body)
+
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
